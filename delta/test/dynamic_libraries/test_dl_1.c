ec2ac43446337f1873c0370b035c9b1982ab1908
Álvaro Goldar Dieste <alvarogoldard@gmail.com>
2020-12-01 01:42:15 +0100

    Added two toy dynamic libraries just for testing purposes

---
+++ delta/test/dynamic_libraries/test_dl_1.c
@@ -0,0 +1,9 @@
+
+#include <math.h>
+
+double test_fun_1(
+    double value
+)
+{
+    return 2.5 * value;
+}
---
+++ delta/test/dynamic_libraries/test_dl_2.c
@@ -0,0 +1,9 @@
+
+#include <math.h>
+
+double test_fun_2(
+    double value
+)
+{
+    return value * value * value;
+}
